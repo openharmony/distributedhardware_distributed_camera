ad03a41cc085e7fc818cde6caf5d3c5818f692ce
wangchaole <wangchaole1@huawei.com>
2022-04-24 17:12:31 +0800

    add fuzztest
    Signed-off-by: wangchaole <wangchaole1@huawei.com>

---
+++ interfaces/inner_kits/native_cpp/test/fuzztest/dcamerasourcehandleripc_fuzzer/dcamera_source_handler_ipc_fuzzer.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "dcamera_source_handler_ipc_fuzzer.h"
+
+#include <stddef.h>
+#include <stdint.h>
+#include "distributed_camera_constants.h"
+#include "dcamera_source_handler.h"
+#include "dcamera_source_handler_ipc.h"
+#include "if_system_ability_manager.h"
+#include "iservice_registry.h"
+
+namespace OHOS {
+namespace DistributedHardware {
+void DCameraSourceHandlerIpcFuzzTest(const uint8_t* data, size_t size)
+{
+    if ((data == nullptr) || (size <= 0)) {
+        return;
+    }
+
+    sptr<ISystemAbilityManager> samgr =
+            SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
+    sptr<IRemoteObject> remoteObject = samgr->GetSystemAbility(DISTRIBUTED_HARDWARE_CAMERA_SOURCE_SA_ID);
+    wptr<IRemoteObject> remote (remoteObject);
+
+    DCameraSourceHandlerIpc::GetInstance().Init();
+    DCameraSourceHandlerIpc::GetInstance().GetSourceLocalDHMS();
+    DCameraSourceHandlerIpc::GetInstance().OnSourceLocalDmsDied(remote);
+    DCameraSourceHandlerIpc::GetInstance().UnInit();
+}
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::DistributedHardware::DCameraSourceHandlerIpcFuzzTest(data, size);
+    return 0;
+}
