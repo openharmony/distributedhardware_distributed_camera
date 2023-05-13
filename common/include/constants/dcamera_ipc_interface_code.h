fec7c45a95647f56b0e8766a94f4e1d98baafd1b
hwzhangchuang <zhangchuang.zhang@huawei.com>
2023-05-13 17:26:27 +0800

    add header content
    Signed-off-by: hwzhangchuang <zhangchuang.zhang@huawei.com>

--- common/include/constants/dcamera_ipc_interface_code.h
+++ common/include/constants/dcamera_ipc_interface_code.h
@@ -12,3 +12,41 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#ifndef OHOS_DCAMERA_IPC_INTERFACE_CODE_H
+#define OHOS_DCAMERA_IPC_INTERFACE_CODE_H
+
+#include <cstdint>
+
+namespace OHOS {
+namespace DistributedHardware {
+    /* SAID: 4803 */
+    enum class IDCameraSourceInterfaceCode : uint32_t {
+        INIT_SOURCE = 0,
+        RELEASE_SOURCE = 1,
+        REGISTER_DISTRIBUTED_HARDWARE = 2,
+        UNREGISTER_DISTRIBUTED_HARDWARE = 3,
+        CAMERA_NOTIFY = 4,
+    };
+
+    /* SAID: 4804 */
+    enum class IDCameraSinkInterfaceCode : uint32_t {
+        INIT_SINK = 0,
+        RELEASE_SINK = 1,
+        SUBSCRIBE_LOCAL_HARDWARE = 2,
+        UNSUBSCRIBE_LOCAL_HARDWARE = 3,
+        STOP_CAPTURE = 4,
+        CHANNEL_NEG = 5,
+        GET_CAMERA_INFO = 6,
+        OPEN_CHANNEL = 7,
+        CLOSE_CHANNEL = 8,
+    };
+
+    /* SAID: 4803 */
+    enum class IDCameraSourceCBInterfaceCode : uint32_t {
+        NOTIFY_REG_RESULT = 0,
+        NOTIFY_UNREG_RESULT = 1,
+    };
+} // namespace DistributedHardware
+} // namespace OHOS
+#endif
