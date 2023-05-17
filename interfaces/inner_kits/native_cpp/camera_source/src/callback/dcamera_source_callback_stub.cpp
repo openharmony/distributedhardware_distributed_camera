2ee726a3719c428af39383128e046942a598d338
hwzhangchuang <zhangchuang.zhang@huawei.com>
2023-05-17 19:55:21 +0800

    modify
    Signed-off-by: hwzhangchuang <zhangchuang.zhang@huawei.com>

--- interfaces/inner_kits/native_cpp/camera_source/src/callback/dcamera_source_callback_stub.cpp
+++ interfaces/inner_kits/native_cpp/camera_source/src/callback/dcamera_source_callback_stub.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,7 +13,6 @@
  * limitations under the License.
  */
 
-#include "dcamera_ipc_interface_code.h"
 #include "dcamera_source_callback_stub.h"
 #include "distributed_camera_errno.h"
 #include "distributed_hardware_log.h"
