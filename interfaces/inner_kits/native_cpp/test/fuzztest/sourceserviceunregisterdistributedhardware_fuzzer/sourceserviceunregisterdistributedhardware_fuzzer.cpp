b15a4a7f19d1f44ca0939fd15da4a77f246cef07
hobbycao <caohui10@huawei.com>
2023-05-23 09:08:43 +0800

    fix: add fuzz test
    Signed-off-by: hobbycao <caohui10@huawei.com>

---
+++ interfaces/inner_kits/native_cpp/test/fuzztest/sourceserviceunregisterdistributedhardware_fuzzer/sourceserviceunregisterdistributedhardware_fuzzer.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "sourceserviceunregisterdistributedhardware_fuzzer.h"
+
+#include "dcamera_source_callback.h"
+#include "distributed_camera_constants.h"
+#include "distributed_camera_source_service.h"
+#include "if_system_ability_manager.h"
+#include "iservice_registry.h"
+
+namespace OHOS {
+namespace DistributedHardware {
+void SourceServiceUnregisterDistributedHardwareFuzzTest(const uint8_t* data, size_t size)
+{
+    if ((data == nullptr) || (size == 0)) {
+        return;
+    }
+
+    std::string dhId(reinterpret_cast<const char*>(data), size);
+    std::string devId(reinterpret_cast<const char*>(data), size);
+    std::string reqId(reinterpret_cast<const char*>(data), size);
+
+    std::shared_ptr<DistributedCameraSourceService> sourceService =
+        std::make_shared<DistributedCameraSourceService>(DISTRIBUTED_HARDWARE_CAMERA_SOURCE_SA_ID, true);
+
+    sourceService->UnregisterDistributedHardware(devId, dhId, reqId);
+}
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::DistributedHardware::SourceServiceUnregisterDistributedHardwareFuzzTest(data, size);
+    return 0;
+}
+
