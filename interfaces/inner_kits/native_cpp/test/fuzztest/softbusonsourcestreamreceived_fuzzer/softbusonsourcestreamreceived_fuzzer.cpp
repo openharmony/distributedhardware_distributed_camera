92d0923abdccc568dfac4c3fc419e5870acc77b5
chen0088 <chenmenghuan6@huawei.com>
2023-06-20 21:09:51 +0800

    fix:add fuzz
    Signed-off-by: chen0088 <chenmenghuan6@huawei.com>

---
+++ interfaces/inner_kits/native_cpp/test/fuzztest/softbusonsourcestreamreceived_fuzzer/softbusonsourcestreamreceived_fuzzer.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "softbusonsourcestreamreceived_fuzzer.h"
+
+#include "session.h"
+#include "single_instance.h"
+#include "dcamera_softbus_session.h"
+
+namespace OHOS {
+namespace DistributedHardware {
+void SoftbusOnSourceStreamReceivedFuzzTest(const uint8_t* data, size_t size)
+{
+    if ((data == nullptr) || (size < sizeof(uint32_t))) {
+        return;
+    }
+
+    int32_t sessionId = *(reinterpret_cast<const int32_t*>(data));
+    const StreamData *data = reinterpret_cast<const StreamData*>(data);
+    const StreamData *ext = reinterpret_cast<const StreamData*>(data);
+    const StreamFrameInfo *param = reinterpret_cast<const StreamData*>(data);
+
+    DCameraSoftbusAdapter::GetInstance().OnSourceStreamReceived(sessionId, data, ext, param);
+}
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::DistributedHardware::SoftbusOnSourceStreamReceivedFuzzTest(data, size);
+    return 0;
+}
+
