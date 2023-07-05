feda323aaccd508d22b3fb0844d6f48d988ab8a6
chen0088 <chenmenghuan6@huawei.com>
2023-07-05 17:31:50 +0800

    fix:modify deps
    Signed-off-by: chen0088 <chenmenghuan6@huawei.com>

--- common/src/utils/dcamera_buffer_handle.cpp
+++ common/src/utils/dcamera_buffer_handle.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -20,7 +20,6 @@
 #include <string>
 #include <sys/mman.h>
 
-#include "buffer_handle_utils.h"
 #include "distributed_hardware_log.h"
 
 namespace OHOS {
