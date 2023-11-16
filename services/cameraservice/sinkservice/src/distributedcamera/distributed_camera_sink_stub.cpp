02416fae63ad46de5049375d2b7644fa9dd9f25d
ry521 <renyi50@huawei.com>
2023-11-16 02:50:44 +0000

    update services/cameraservice/sinkservice/src/distributedcamera/distributed_camera_sink_stub.cpp.
    Signed-off-by: ry521 <renyi50@huawei.com>

--- services/cameraservice/sinkservice/src/distributedcamera/distributed_camera_sink_stub.cpp
+++ services/cameraservice/sinkservice/src/distributedcamera/distributed_camera_sink_stub.cpp
@@ -100,14 +100,15 @@ int32_t DistributedCameraSinkStub::InitSinkInner(MessageParcel &data, MessagePar
 int32_t DistributedCameraSinkStub::ReleaseSinkInner(MessageParcel &data, MessageParcel &reply)
 {
     DHLOGD("enter");
-    int32_t ret;
-    if (!HasEnableDHPermission()) {
-        DHLOGE("The caller has no ENABLE_DISTRIBUTED_HARDWARE permission.");
-        ret = DCAMERA_BAD_VALUE;
-        reply.WriteInt32(ret);
-        return DCAMERA_BAD_VALUE;
-    }
-    ret = ReleaseSink();
+    int32_t ret = DCAMERA_OK;
+    do {
+        if (!HasEnableDHPermission()) {
+            DHLOGE("The caller has no ENABLE_DISTRIBUTED_HARDWARE permission.");
+            ret = DCAMERA_BAD_VALUE;
+            break;
+        }
+        ret = ReleaseSink();
+    } while (0);    
     reply.WriteInt32(ret);
     return DCAMERA_OK;
 }
