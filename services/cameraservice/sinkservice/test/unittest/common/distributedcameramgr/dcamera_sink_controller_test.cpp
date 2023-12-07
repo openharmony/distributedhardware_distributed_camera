51cea1a0c1de4e6812517f823e00a544ddb57202
ry521 <renyi50@huawei.com>
2023-12-07 02:50:10 +0000

    .
    Signed-off-by: ry521 <renyi50@huawei.com>

--- services/cameraservice/sinkservice/test/unittest/common/distributedcameramgr/dcamera_sink_controller_test.cpp
+++ services/cameraservice/sinkservice/test/unittest/common/distributedcameramgr/dcamera_sink_controller_test.cpp
@@ -148,12 +148,13 @@ void DCameraSinkControllerTest::TearDown(void)
 void DCameraSinkControllerTest::SetTokenID()
 {
     uint64_t tokenId;
-    const char *perms[2];
+    int32_t numberOfPermissions = 2;
+    const char *perms[numberOfPermissions];
     perms[0] = "ohos.permission.DISTRIBUTED_DATASYNC";
     perms[1] = "ohos.permission.CAMERA";
     NativeTokenInfoParams infoInstance = {
         .dcapsNum = 0,
-        .permsNum = 2,
+        .permsNum = numberOfPermissions,
         .aclsNum = 0,
         .dcaps = NULL,
         .perms = perms,
