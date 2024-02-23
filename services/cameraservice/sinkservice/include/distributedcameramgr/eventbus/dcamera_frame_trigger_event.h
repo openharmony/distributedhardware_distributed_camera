348f6a254cfbc6e23a4aa868b614c11f9c0a8fe9
chen0088 <chenmenghuan6@huawei.com>
2024-02-23 17:30:37 +0800

    modify gn to support independent complilation
    Signed-off-by: chen0088 <chenmenghuan6@huawei.com>

--- services/cameraservice/sinkservice/include/distributedcameramgr/eventbus/dcamera_frame_trigger_event.h
+++ services/cameraservice/sinkservice/include/distributedcameramgr/eventbus/dcamera_frame_trigger_event.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,7 +17,7 @@
 #define OHOS_DCAMERA_FRAME_TRIGER_EVENT_H
 
 #include <string>
-#include "event.h"
+#include "eventbus/event.h"
 
 namespace OHOS {
 namespace DistributedHardware {
