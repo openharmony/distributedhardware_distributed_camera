e3bf6874a94d5d4f70ecea8ef84ab994e8c5baf3
w30042960 <wangyupan1@huawei.com>
2024-08-20 10:46:12 +0800

    modify ffrt
    Signed-off-by: w30042960 <wangyupan1@huawei.com>

--- services/cameraservice/sinkservice/src/distributedcameramgr/dcamera_sink_controller.cpp
+++ services/cameraservice/sinkservice/src/distributedcameramgr/dcamera_sink_controller.cpp
@@ -36,6 +36,7 @@
 #include "distributed_hardware_log.h"
 #include "device_security_defines.h"
 #include "device_security_info.h"
+#include "ffrt_inner.h"
 #include "idistributed_camera_source.h"
 #include "ipc_skeleton.h"
 #include "dcamera_low_latency.h"
@@ -472,7 +473,7 @@ void DCameraSinkController::OnSessionState(int32_t state)
             break;
         case DCAMERA_CHANNEL_STATE_DISCONNECTED:
             DHLOGI("channel is disconnected");
-            std::thread([this]() {
+            ffrt::submit([this]() {
                 DHLOGI("DCameraSinkController::OnSessionState %{public}s new thread session state: %{public}d",
                     GetAnonyString(dhId_).c_str(), sessionState_);
                 prctl(PR_SET_NAME, CHANNEL_DISCONNECTED.c_str());
@@ -487,7 +488,7 @@ void DCameraSinkController::OnSessionState(int32_t state)
                     DHLOGE("session state: %{public}d, %{public}s stop capture failed, ret: %{public}d",
                         sessionState_, GetAnonyString(dhId_).c_str(), ret);
                 }
-            }).detach();
+            });
             break;
         default:
             DHLOGE("unknown session state");
