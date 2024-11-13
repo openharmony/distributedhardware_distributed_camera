91103ac97970fd6f7b17358f186520ffe7a33f51
chen0088 <chenmenghuan6@huawei.com>
2024-11-13 16:14:58 +0800

    add ut
    Signed-off-by: chen0088 <chenmenghuan6@huawei.com>

--- services/data_process/test/unittest/common/pipeline/dcamera_pipeline_source_test.cpp
+++ services/data_process/test/unittest/common/pipeline/dcamera_pipeline_source_test.cpp
@@ -86,6 +86,9 @@ HWTEST_F(DCameraPipelineSourceTest, dcamera_pipeline_source_test_001, TestSize.L
                                  TEST_HEIGTH2);
     int32_t rc = testSourcePipeline_->CreateDataProcessPipeline(PipelineType::VIDEO, srcParams, destParams, listener);
     EXPECT_EQ(rc, DCAMERA_OK);
+
+    rc = testSourcePipeline_->CreateDataProcessPipeline(PipelineType::VIDEO, srcParams, destParams, nullptr);
+    EXPECT_EQ(rc, DCAMERA_BAD_VALUE);
     usleep(SLEEP_TIME);
 }
 
