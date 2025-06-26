d4c3b3c442e83c6664d39ade3937219832cfe2d7
冯俊桦 <fengjunhua4@h-partners.com>
2025-06-26 16:54:44 +0800

    相机fuzz补充
    Signed-off-by: 冯俊桦 <fengjunhua4@h-partners.com>

--- interfaces/inner_kits/native_cpp/test/sourcefuzztest/softbusadapter_fuzzer/softbusadapter_fuzzer.cpp
+++ interfaces/inner_kits/native_cpp/test/sourcefuzztest/softbusadapter_fuzzer/softbusadapter_fuzzer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -21,7 +21,7 @@ namespace OHOS {
 namespace DistributedHardware {
 void SoftbusCreatSoftBusSinkSocketServerFuzzTest(const uint8_t* data, size_t size)
 {
-    if (data == nullptr) {
+    if (data == nullptr || size == 0) {
         return;
     }
 
